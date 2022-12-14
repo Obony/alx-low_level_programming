0f840a8fb1cf73b56a0b849526bc84cc309737b6
Obony <faithnyabz69@gmail.com>
2022-12-14 02:44:22 -0800

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,19 +2,16 @@
 
 /**
 *main - Test function for positive or negative
-*
 *Return: 0
 */
 
 int main(void)
 {
-	int i;
+int i;
+i = 0;
+positive_or_negative(i);
 
-	i = 0;
-
-	positive_or_negative(i);
-
-	return (0);
+return (0);
 }
 
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
+#ifndef main_h
+#define main_h
 #include <stdio.h>
-
-
 
 void positive_or_negative(int i);
 
@@ -13,9 +9,6 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
-
-
-
 #endif
 
 
