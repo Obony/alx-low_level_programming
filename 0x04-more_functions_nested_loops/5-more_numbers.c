e478e3124b96936e02d395e555f150bc14cd05ee
Obony <faithnyabz69@gmail.com>
2022-12-15 03:06:29 -0800

    print numbers from 0 to 9 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
-			_putchar(j % 10 + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
