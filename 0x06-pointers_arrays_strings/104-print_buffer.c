8fdcc82ca95281f82dafabc6ed211e2ce676b40c
Obony <faithnyabz69@gmail.com>
2022-12-21 10:07:25 -0800

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_buffer - prints buffer
@@ -13,7 +12,7 @@ void print_buffer(char *b, int size)
 
 	if (size < 0)
 	{
-		printf('\n');
+		printf("\n");
 		return;
 	}
 	while (i < size)
@@ -38,7 +37,7 @@ void print_buffer(char *b, int size)
 		else
 			printf(".");
 	}
-	printf('\n');
+	printf("\n");
 	i += 10;
 }
 
