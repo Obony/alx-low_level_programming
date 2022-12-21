b8190a940bb3f8149ed5a97bd9ef71d6f240273e
Obony <faithnyabz69@gmail.com>
2022-12-21 13:20:16 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-/**
-  *main - entry
-  *Return: 0
-  */
+
 int main(void)
 {
 	int n;
@@ -19,7 +16,7 @@ int main(void)
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
-	p[5] = 98;
+	*p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
