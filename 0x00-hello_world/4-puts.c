b8f9f4336a89342751437e2a53a4129acae99b68
Obony <faithnyabz69@gmail.com>
2022-12-21 05:18:55 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Print a line of code using puts
+ * main - Entry
  *
  * Return: Always 0 (Success)
-i*/
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
