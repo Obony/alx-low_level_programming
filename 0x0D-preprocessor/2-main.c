363cfcabf9db11174a829b6bdb7d977773d76515
Obony <faithnyabz69@gmail.com>
2023-01-17 02:19:36 -0800

    Macro FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: Prints the name of the current file
+ * Desc: Using macro __FILE__.
+ * Return: 0 on success
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
