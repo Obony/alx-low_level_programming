d03aa1e65f0618515f407979390df0014834a95b
Obony <faithnyabz69@gmail.com>
2023-01-17 03:05:21 -0800

    Poppy dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,13 +12,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/**
- * dog - Typdef for dog type
- */
-
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
