2adf14f0b0efe7eae680254598bf25b00f5932fc
Obony <faithnyabz69@gmail.com>
2023-01-17 02:42:34 -0800

    Function like macro, ABS(x)

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,11 +2,12 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /**
- * File - 3-function_like_macro.h
+ * File: 3-function_like_macro.h
  * Desc: A macro ABS(x) that returns the absolute
  *       value of an x
- * @x:the integer
+ * @x: the integer
  */
-#define ABS(x) (((x) < 0) ? (-x) : (x))
+
+#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
 
 #endif
