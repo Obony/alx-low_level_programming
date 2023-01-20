fab7aadae9b8d5937c0e94ba0a622e04da6cee70
Obony <faithnyabz69@gmail.com>
2023-01-20 07:01:01 -0800

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,12 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+#include <stdarg.h>
+#include <stdio.h>
 
-typedef struct validTypes
-{
-	char *valid;
-	void (*f)();
-} v_types;
-
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+int _putchar(char c);
 #endif
