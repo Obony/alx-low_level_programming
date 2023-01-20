916ac7a17deb0092a5b9dd4f9720ca7dfeef44c3
Obony <faithnyabz69@gmail.com>
2023-01-20 01:36:08 -0800

    variadic task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,7 +10,7 @@ typedef struct printer
 	void (*print)(va_list arg);
 } printer_t;
 
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
