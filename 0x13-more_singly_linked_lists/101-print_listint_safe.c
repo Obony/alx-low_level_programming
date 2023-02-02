0b88c98bf305c35780b68cab276443a3d0cb35e3
Obony <faithnyabz69@gmail.com>
2023-02-01 16:41:49 -0800

    print(safe version) a list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,11 +6,11 @@ size_t print_listint_safe(const listint_t *head);
 
 /**
  * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
+ *                      in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
  * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ *         Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
@@ -19,6 +19,7 @@ size_t looped_listint_len(const listint_t *head)
 
 	if (head == NULL || head->next == NULL)
 		return (0);
+
 	tortoise = head->next;
 	hare = (head->next)->next;
 
@@ -27,25 +28,27 @@ size_t looped_listint_len(const listint_t *head)
 		if (tortoise == hare)
 		{
 			tortoise = head;
-
 			while (tortoise != hare)
 			{
 				nodes++;
 				tortoise = tortoise->next;
 				hare = hare->next;
 			}
-			tortoise = tortoise->next;
 
+			tortoise = tortoise->next;
 			while (tortoise != hare)
 			{
 				nodes++;
 				tortoise = tortoise->next;
 			}
+
 			return (nodes);
 		}
+
 		tortoise = tortoise->next;
 		hare = (hare->next)->next;
 	}
+
 	return (0);
 }
 
@@ -58,6 +61,7 @@ size_t looped_listint_len(const listint_t *head)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes, index = 0;
+
 	nodes = looped_listint_len(head);
 
 	if (nodes == 0)
@@ -68,6 +72,7 @@ size_t print_listint_safe(const listint_t *head)
 			head = head->next;
 		}
 	}
+
 	else
 	{
 		for (index = 0; index < nodes; index++)
@@ -75,7 +80,9 @@ size_t print_listint_safe(const listint_t *head)
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
+
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
+
 	return (nodes);
 }
